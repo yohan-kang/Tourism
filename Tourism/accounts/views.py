263cd630ce344da854a9b4c0bc829e1dbf1f5b98
Louis <louis.nourigeon@gmail.com>
2022-07-24 17:38:31 +0900

    add debug condition

--- Tourism/accounts/views.py
+++ Tourism/accounts/views.py
@@ -1,9 +1,10 @@
+from django.conf import settings
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.http import Http404
-
 # Create your views here.
 def list(request):
     # 로그인 상태 표시 : ㄴis_authenticated
-    print(request.user.is_authenticated) 
+    if settings.DEBUG:
+        print(request.user.is_authenticated) 
     return render(request,'tourism/list.html')
