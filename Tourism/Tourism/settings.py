02dc32999ec779f2f4b512b16ce12866b2426b5c
Louis <louis.nourigeon@gmail.com>
2022-07-25 15:41:37 +0900

    added docker file

--- Tourism/Tourism/settings.py
+++ Tourism/Tourism/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = str(os.environ.get("DJANGO_SECRET_KEY"))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = str(os.environ.get("DEBUG")) == '1'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1','localhost']
 
 
 # Application definition
