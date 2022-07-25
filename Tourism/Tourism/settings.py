bb2c49c8c6516b31432ba9e3bf58ab87aa983b9b
DESKTOP-5S8AV37\LABSD_2 <rmvmf123@labsd.net>
2022-07-25 10:11:21 +0900

    secret key

--- Tourism/Tourism/settings.py
+++ Tourism/Tourism/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-t!m+=wxnn0eilofdfr7&st0$72um*uq2x#(8jktq27rldc%p8h'
+SECRET_KEY = str(os.environ.get("DJANGO_SECRET_KEY"))
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = str(os.environ.get("DEBUG")) == '1'
