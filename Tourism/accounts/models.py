bfe4e7a19f2a6dbb275f5eb8f901f9520b2537dc
DESKTOP-5S8AV37\LABSD_2 <rmvmf123@labsd.net>
2022-07-25 11:36:22 +0900

    Set User Subscription Nickname

--- Tourism/accounts/models.py
+++ Tourism/accounts/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
+    nickname = models.CharField(max_length=15,unique=True , null=True)
     pass
   # only email login case 
   #   def __srt__(self):
