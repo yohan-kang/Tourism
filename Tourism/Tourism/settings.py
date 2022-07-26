e88099f058ac8756d4aaaf0840305f5ce79586d6
Louis <louis.nourigeon@gmail.com>
2022-07-26 12:10:50 +0900

    test

--- Tourism/Tourism/settings.py
+++ Tourism/Tourism/settings.py
@@ -81,10 +81,10 @@ WSGI_APPLICATION = 'Tourism.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
 }
 
 DB_USERNAME = os.environ.get("POSTGRES_USERNAME")
