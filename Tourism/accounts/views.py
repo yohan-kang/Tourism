04990400002e7e8f7079d798d552b9f87730fd9c
DESKTOP-5S8AV37\LABSD_2 <rmvmf123@labsd.net>
2022-08-03 14:23:51 +0900

    accounts app view setting

--- Tourism/accounts/views.py
+++ Tourism/accounts/views.py
@@ -1,13 +1,5 @@
-from django.conf import settings
-from django.shortcuts import render
-from django.shortcuts import reverse
 from django.http import JsonResponse 
-from django.http import HttpResponse
-from django.http import Http404
-from allauth.account.views import PasswordChangeView
-from rest_framework import viewsets
-from rest_framework import serializers
-from accounts.models import User
+from rest_framework.response import Response
 
 
 def getRoutes(requset):
@@ -17,21 +9,32 @@ def getRoutes(requset):
     ]
     return JsonResponse(routes,safe=False)
 
-# Create your views here.
-def list(request):
-    # 로그인 상태 표시 : ㄴis_authenticated
-    if settings.DEBUG:
-        print(request.user.is_authenticated) 
-    return render(request,'tourism/list.html')
+# from django.conf import settings
+# from django.shortcuts import render
+# from django.shortcuts import reverse
+# from django.http import HttpResponse
+# from django.http import Http404
+# from allauth.account.views import PasswordChangeView
+# from rest_framework import viewsets
+# from rest_framework import serializers
+# from accounts.models import User
 
-class CustomPasswordChangeView(PasswordChangeView):
-    def get_success_url(self):
-      return reverse("list")
 
-class UserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = "__all__"
-class UserViewSet(viewsets.ModelViewSet):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
+# # Create your views here.
+# def list(request):
+#     # 로그인 상태 표시 : ㄴis_authenticated
+#     if settings.DEBUG:
+#         print(request.user.is_authenticated) 
+#     return render(request,'tourism/list.html')
+
+# class CustomPasswordChangeView(PasswordChangeView):
+#     def get_success_url(self):
+#       return reverse("list")
+
+# class UserSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = User
+#         fields = "__all__"
+# class UserViewSet(viewsets.ModelViewSet):
+#     queryset = User.objects.all()
+#     serializer_class = UserSerializer
