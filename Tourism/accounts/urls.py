215fa506512f36433a338100a2eecf51b3a0b657
DESKTOP-5S8AV37\LABSD_2 <rmvmf123@labsd.net>
2022-08-03 14:53:39 +0900

    install djangorestframework-simplejwt

--- Tourism/accounts/urls.py
+++ Tourism/accounts/urls.py
@@ -1,8 +1,16 @@
 from django.urls import path, include
 from . import views
+from rest_framework_simplejwt.views import(
+  TokenObtainPairView,
+  TokenRefreshView,
+  TokenVerifyView
+)
 
 urlpatterns = [
-    path('',views.getRoutes)
+    path('',views.getRoutes),
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+
 ]
 
 # from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
