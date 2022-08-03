7e263fd4719934db2c62a44bc8c9e013b6446516
DESKTOP-5S8AV37\LABSD_2 <rmvmf123@labsd.net>
2022-08-03 14:13:42 +0900

    change url setting

--- Tourism/Tourism/urls.py
+++ Tourism/Tourism/urls.py
@@ -1,50 +1,41 @@
-"""Tourism URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path,include
-from accounts import views 
-from django.views.generic import TemplateView
-from accounts.views import CustomPasswordChangeView
-from rest_framework import routers
-from accounts.views import UserViewSet
 
-router = routers.DefaultRouter()
-router.register('user', UserViewSet)
 urlpatterns = [
-    path('', include(router.urls)),
-    # admin
     path('admin/', admin.site.urls),
-    # accounts
-    # path('accounts/', include('dj_rest_auth.urls')),
-    path('accounts/', include('dj_rest_auth.registration.urls')),
-    # path('accounts/', include('allauth.urls')),
-    path('accounts/', include('accounts.urls')),
-    #board
-    path("", include('board.urls')),
-    # allauth
-    path(
-      "email-confirmation-done/", 
-      TemplateView.as_view(template_name='Tourism/email_confirmation_done.html'),
-      name="account_email_confirmation_done",
-    ),
-    path(
-        'password/change/',
-        CustomPasswordChangeView.as_view(),
-        name="account_password_change"
-    ),
-    # list page
-    path('accommodation/', views.list),
+    path('api/', include('accounts.urls')),
 ]
+
+# from accounts import views 
+# from django.views.generic import TemplateView
+# from accounts.views import CustomPasswordChangeView
+# from rest_framework import routers
+# from accounts.views import UserViewSet
+
+# router = routers.DefaultRouter()
+# router.register('user', UserViewSet)
+# urlpatterns = [
+#     path('', include(router.urls)),
+#     # admin
+#     path('admin/', admin.site.urls),
+#     # accounts
+#     # path('accounts/', include('dj_rest_auth.urls')),
+#     path('accounts/', include('dj_rest_auth.registration.urls')),
+#     # path('accounts/', include('allauth.urls')),
+#     path('accounts/', include('accounts.urls')),
+#     #board
+#     path("", include('board.urls')),
+#     # allauth
+#     path(
+#       "email-confirmation-done/", 
+#       TemplateView.as_view(template_name='Tourism/email_confirmation_done.html'),
+#       name="account_email_confirmation_done",
+#     ),
+#     path(
+#         'password/change/',
+#         CustomPasswordChangeView.as_view(),
+#         name="account_password_change"
+#     ),
+#     # list page
+#     path('accommodation/', views.list),
+# ]
--- Tourism/accounts/urls.py
+++ Tourism/accounts/urls.py
@@ -3,12 +3,13 @@ from . import views
 # from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
 from rest_framework_simplejwt import views as jwt_views
 urlpatterns = [
-    path('', include('dj_rest_auth.urls')),
-    # path('', views.list, name="list"),
-    # path('login/', views.JWTLoginView.as_view(), name="list"),
-    # path('signup/', views.JWTSignupView.as_view(), name="list"),
-    path('registration/', include('dj_rest_auth.registration.urls')),
-    path('token/', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
-    path('token/verify/', jwt_views.TokenVerifyView.as_view(), name='token_verify'),
+    path('',views.getRoutes)
+    # path('', include('dj_rest_auth.urls')),
+    # # path('', views.list, name="list"),
+    # # path('login/', views.JWTLoginView.as_view(), name="list"),
+    # # path('signup/', views.JWTSignupView.as_view(), name="list"),
+    # path('registration/', include('dj_rest_auth.registration.urls')),
+    # path('token/', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    # path('token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
+    # path('token/verify/', jwt_views.TokenVerifyView.as_view(), name='token_verify'),
 ]
