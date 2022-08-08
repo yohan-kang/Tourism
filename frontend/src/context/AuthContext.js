916ba02455ea7515e91f44a909c5d30d197dc4f0
DESKTOP-5S8AV37\LABSD_2 <rmvmf123@labsd.net>
2022-08-08 10:33:05 +0900

    Refresh & Rotating Tokens

--- frontend/src/context/AuthContext.js
+++ frontend/src/context/AuthContext.js
@@ -1,4 +1,4 @@
-import { createContext, useState, userEffect } from 'react'
+import { createContext, useState,useEffect } from 'react'
 import jwt_decode from 'jwt-decode'
 import { useHistory } from 'react-router-dom'
 
@@ -7,10 +7,11 @@ const AuthContext = createContext()
 export default AuthContext;
 
 export const AuthProvider = ({children}) => {
-  
+  console.log('update token call')
   // localStorge.getItem('authTokens') ? JSON.parse(localStorage.getItem('authTokens')) : null
   let [authTokens, setAuthTokens] =useState(() =>  localStorage.getItem('authTokens') ? JSON.parse(localStorage.getItem('authTokens')) : null)
   let [user, setUser] = useState(()=> localStorage.getItem('authTokens') ? jwt_decode(localStorage.getItem('authTokens')) : null)
+  let [loading, setLoading] = useState(true)
 
   const history = useHistory()
 
@@ -43,11 +44,44 @@ export const AuthProvider = ({children}) => {
     history.push('/login')
   }
 
+  let updateToken = async ()=> {
+    let response = await fetch('http://127.0.0.1:8000/api/token/refresh/', {
+      method : 'POST',
+      headers:{
+          'Content-Type':'application/json'
+      },
+      body:JSON.stringify({'refresh':authTokens.refresh})
+    })
+    let data = await response.json()
+
+    if (response.status === 200) {
+      setAuthTokens(data)
+      setUser(jwt_decode(data.access))
+      localStorage.setItem('authTokens',JSON.stringify(data))
+    } else {
+        logoutUser()
+    }
+  }
+
+
   let contextData = {
       user:user,
       loginUser:loginUser,
       logoutUser:logoutUser,
   }
+
+  useEffect(() => {
+
+      let fourMinutes = 1000 * 60 * 4
+      let interval = setInterval(()=> {
+          if(authTokens){
+            updateToken()
+          }
+      },fourMinutes)
+      return ()=> clearInterval(interval)
+      
+  },[authTokens,loading])
+
 
   return (
     // <AuthContext.Provider value={{'name':'yohan'}} >
