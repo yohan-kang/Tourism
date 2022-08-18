6852c2c286c0ccdf7450c493d558d4fef331cb45
DESKTOP-5S8AV37\LABSD_2 <rmvmf123@labsd.net>
2022-08-18 16:21:17 +0900

    test code add :  crud

--- Tourism/board/tests/tests_views.py
+++ Tourism/board/tests/tests_views.py
@@ -28,4 +28,35 @@ class TestBoardViewMethods(APITestCase, URLPatternsTestCase):
     self.assertEqual(board["id"], 2)
     self.assertEqual(board["title"], "board2")
 
-  
+  def test_should_create_newboard(self):
+    url = reverse('boardInsert')
+    response = self.client.post(url,data={"title":"board3","writer":"writer3","date":"2022-08-01T02:00:00Z"})
+    board = response.data
+    # print(board)
+    self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+    self.assertEqual(board["title"],"board3")
+    url = reverse('boardList')
+    response = self.client.get(url, format='json')
+    self.assertEqual(response.status_code, status.HTTP_200_OK)
+    self.assertEqual(len(response.data), 3)
+
+  def test_should_delete_board2(self):
+    url = reverse('boardDelete',kwargs={"pk":"2"})
+    response = self.client.delete(url)
+    print("---- delete ----")
+    print(response.status_code)
+    url = reverse('boardList')
+    response = self.client.get(url, format='json')
+    print(len(response.data))
+    self.assertEqual(len(response.data), 1)
+
+  def test_should_update_board1(self):
+    print("---- update ----")
+    url = reverse('boardList')
+    response = self.client.get(url, format='json')
+    print(len(response.data))
+    url = reverse('boardView',kwargs={"pk":"1"})
+    response = self.client.put(url,data={"title":"board1change","writer":"writer100","date":"2022-08-01T02:00:00Z"})
+    url = reverse('boardList')
+    response = self.client.get(url, format='json')
+    print(response.data)
