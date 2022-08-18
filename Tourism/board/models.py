f11c432041b861cb24c9b94b022144c2446e180c
DESKTOP-5S8AV37\LABSD_2 <rmvmf123@labsd.net>
2022-08-18 17:54:32 +0900

    setting Foreign key

--- Tourism/board/models.py
+++ Tourism/board/models.py
@@ -1,9 +1,13 @@
 from django.db import models
+from django.conf import settings
+
+User = settings.AUTH_USER_MODEL
 
 class Board(models.Model):
     b_title = models.CharField(db_column='b_title', max_length=255)
     b_writer = models.CharField(db_column='b_writer', max_length=50)
     b_date = models.DateTimeField(db_column='b_date')
+    accessUser = models.ForeignKey(User, default=1, null=True, on_delete=models.SET_NULL)
 
     class Meta:
         managed = True
--- Tourism/board/serializers.py
+++ Tourism/board/serializers.py
@@ -5,10 +5,11 @@ class BoardSerializer(serializers.ModelSerializer):
     title = serializers.CharField(source="b_title")
     writer = serializers.CharField(source="b_writer")
     date = serializers.DateTimeField(source="b_date")
+    user = serializers.CharField(source="accessUser")
 
     class Meta:
         model=Board
-        fields = ['id','title','writer','date']
+        fields = ['user','id','title','writer','date']
         # fields = "__all__"
         # fields = ('b_title', 'b_writer', 'b_date')
 
