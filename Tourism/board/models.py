7a070d52d002a2b9e3b4ccb4cb6b99914182dec9
DESKTOP-5S8AV37\LABSD_2 <rmvmf123@labsd.net>
2022-08-19 13:05:44 +0900

    model default part remove

--- Tourism/board/models.py
+++ Tourism/board/models.py
@@ -7,7 +7,7 @@ class Board(models.Model):
     b_title = models.CharField(db_column='b_title', max_length=255)
     b_writer = models.CharField(db_column='b_writer', max_length=50)
     b_date = models.DateTimeField(db_column='b_date')
-    accessUser = models.ForeignKey(User, default=1, null=True, on_delete=models.SET_NULL)
+    accessUser = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
 
     class Meta:
         managed = True
