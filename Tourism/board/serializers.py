e9919cf2d8ff7508ad7cad1b8c3055d091722115
DESKTOP-5S8AV37\LABSD_2 <rmvmf123@labsd.net>
2022-08-26 18:25:34 +0900

    serializers test

--- Tourism/board/serializers.py
+++ Tourism/board/serializers.py
@@ -12,7 +12,12 @@ class ImgSerializer(serializers.ModelSerializer):
 class BoardSerializer(serializers.ModelSerializer):
 
     # reviewimgs = ImgSerializer(many=True, read_only=True)
-    # img_list = serializers.SerializerMethodField('_get_imgs')
+    img_list = serializers.SerializerMethodField('_get_imgs')
+
+    def _get_imgs(self, obj):
+
+      serializer = ImgSerializer(obj.reviewimg_set.all(),many=True)
+      return serializer.data
 
     def to_representation(self, instance):
       res = super().to_representation(instance)
@@ -21,8 +26,8 @@ class BoardSerializer(serializers.ModelSerializer):
 
     class Meta:
         model=Board
-        fields = ['id','title','writer','content','created_at','updated_at','reviewimg_set']
-        read_only_fields = ['writer','created_at','updated_at','reviewimg_set']
+        fields = ['id','title','writer','content','created_at','updated_at','img_list']
+        read_only_fields = ['writer','created_at','updated_at','img_list']
         # lookup_field = 'writer'
         # read_only_fields = ['created_at']
         # fields = "__all__"
