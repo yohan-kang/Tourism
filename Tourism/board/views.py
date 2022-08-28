e8c8ff8b37074d0c7ee2375cd661193b9084b895
DESKTOP-5S8AV37\LABSD_2 <rmvmf123@labsd.net>
2022-08-28 20:23:19 +0900

    views.py import remove : unused part

--- Tourism/board/views.py
+++ Tourism/board/views.py
@@ -1,32 +1,24 @@
-from itertools import product
-from urllib import request
-from django.shortcuts import render
-
 # Create your views here.
-from django.shortcuts import render
-from django.http import JsonResponse
-from rest_framework.authentication import TokenAuthentication
-from .models import Board
-from .models import ReviewImg
+from django.conf import settings
+from .models import Board, ReviewImg
 from .serializers import BoardSerializer,ImgSerializer
 from django.core.cache import cache
 from rest_framework.permissions import IsAuthenticated
 
-# --api_view case import add--
-from rest_framework.response import Response
-from rest_framework.decorators import api_view, permission_classes, authentication_classes
-from django.http import Http404
-
 # --APIView(generics),permission case import add--
-# from rest_framework.views import APIView
-from rest_framework import generics,permissions,authentication
-# from Tourism.board import serializers
+from rest_framework import generics,permissions
 
 # custom permissions
-from .permissions import IsStaffEditorPermission, IsTechnicianPermission
-# custom mixins
-from .mixins import StaffEditorPermissionMixin
-from django.conf import settings
+# from .permissions import IsStaffEditorPermission, IsTechnicianPermission
+
+
+# unused
+# from itertools import product
+# from urllib import request
+# from django.shortcuts import render
+# from django.http import JsonResponse
+# from rest_framework.response import Response
+# from django.http import Http404
 
 User = settings.AUTH_USER_MODEL
 
@@ -35,14 +27,12 @@ User = settings.AUTH_USER_MODEL
 #     serializer_class = BoardSerializer
 # board_view = somethingAPIView.as_view()
 
-
-
 class BoardAllList(generics.ListAPIView):
   queryset = Board.objects.all()
   serializer_class = BoardSerializer
 
 class BoardWriterList(generics.ListCreateAPIView):
-  permission_classes = [permissions.IsAdminUser,IsStaffEditorPermission]
+  permission_classes = [permissions.IsAdminUser]
   # permission_classes = [permissions.DjangoModelPermissions ]
   serializer_class = BoardSerializer
   def get_queryset(self):
@@ -50,8 +40,6 @@ class BoardWriterList(generics.ListCreateAPIView):
     return Board.objects.filter(writer=user)
   def perform_create(self, serializer):
     serializer.save(writer=self.request.user)
-  
-
 
 class BoardDetail(generics.RetrieveUpdateDestroyAPIView):
   permission_classes = [IsAuthenticated]
